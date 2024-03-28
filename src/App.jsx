36b42c9a1614a0a4c13bcaaeb35eeb32951a0c7c
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-28 23:12:07 +0200

    Added List page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
-import HomePage from './routes/homePage/homePage';
 import Layout from './routes/layout/layout';
+import HomePage from './routes/homePage/homePage';
+import ListPage from './routes/listPage/listPage';
 
 function App() {
     const router = createBrowserRouter([
@@ -11,6 +12,10 @@ function App() {
                 {
                     path: '/',
                     element: <HomePage />
+                },
+                {
+                    path: '/list',
+                    element: <ListPage />
                 }
             ]
         }
---
+++ src/routes/listPage/listPage.jsx
@@ -0,0 +1,15 @@
+import './listPage.scss';
+
+function ListPage() {
+
+    return <div className="listPage">
+        <div className="listContainer">
+            <div className="wrapper">
+            </div>
+        </div>
+        <div className="mapContainer">
+        </div>
+    </div>;
+}
+
+export default ListPage;
