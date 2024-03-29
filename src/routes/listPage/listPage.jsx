e3d5f85e8c10de5b2fd878100c402be6e161cbf2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-03-29 22:42:58 +0200

    Update List page

--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -1,13 +1,24 @@
+import { listData } from '../../lib/dummydata';
+import Filter from '../../component/filter/Filter';
+import Card from '../../component/card/Card';
+import Map from '../../component/map/Map';
+
 import './listPage.scss';
 
 function ListPage() {
+    const data = listData;
 
     return <div className="listPage">
         <div className="listContainer">
             <div className="wrapper">
+                <Filter />
+                {data.map(item => (
+                    <Card key={item.id} item={item} />
+                ))}
             </div>
         </div>
         <div className="mapContainer">
+            <Map items={data} />
         </div>
     </div>;
 }
