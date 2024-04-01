76d26c509c87e365668205702ddbb7ad90a236d8
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-01 22:56:04 +0300

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import HomePage from './routes/homePage/homePage';
 import ListPage from './routes/listPage/listPage';
 import SinglePage from './routes/singlePage/singlePage';
 import ProfilePage from './routes/profilePage/profilePage';
+import Register from './routes/register/register';
+import Login from './routes/login/login';
+import ProfileUpdatePage from './routes/profileUpdatePage/profileUpdatePage';
 
 function App() {
     const router = createBrowserRouter([
@@ -26,6 +29,18 @@ function App() {
                 {
                     path: '/profile',
                     element: <ProfilePage />
+                },
+                {
+                    path: '/profile/update',
+                    element: <ProfileUpdatePage />,
+                },
+                {
+                    path: '/register',
+                    element: <Register />
+                },
+                {
+                    path: '/login',
+                    element: <Login />
                 }
             ]
         }
