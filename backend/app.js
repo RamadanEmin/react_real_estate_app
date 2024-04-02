3d6dbd50f3e84dc266b30790ab0c98867fe818a2
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-02 23:18:33 +0300

    Added Logout controller

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import authRoute from './routes/auth.route.js';
 
 const app = express();
 
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
