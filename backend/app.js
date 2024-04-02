28bcfbea58ba1ee175f1796299300f7de8a446d3
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-02 23:12:49 +0300

    Added Login controller

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import cookieParser from "cookie-parser";
 import authRoute from './routes/auth.route.js';
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 
--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
@@ -23,5 +24,49 @@ export const register = async (req, res) => {
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Failed to create user!" });
+    }
+};
+
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { username }
+        });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials!" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid Credentials!" });
+        }
+
+        const age = 1000 * 60 * 60 * 24 * 7;
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                isAdmin: false
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: age }
+        );
+
+        const { password: userPassword, ...userInfo } = user;
+
+        res
+            .cookie("token", token, {
+                httpOnly: true,
+                maxAge: age
+            })
+            .status(200)
+            .json(userInfo);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Failed to login!" });
     }
 };
