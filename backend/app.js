d095edbd3be8010975adb1c9762e4e67ce98dac6
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-03 17:15:55 +0300

    Added testing route for login users

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoute from './routes/auth.route.js';
+import testRoute from "./routes/test.route.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8800, () => {
     console.log('Server is running!');
---
+++ backend/controllers/test.controller.js
@@ -0,0 +1,4 @@
+export const shouldBeLoggedIn = async (req, res) => {
+    console.log(req.userId)
+    res.status(200).json({ message: 'You are Authenticated' });
+};
