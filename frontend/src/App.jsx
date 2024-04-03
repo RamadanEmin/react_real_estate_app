b29cff441507ffaf5c1a04c1a1239bdac0e1eb8e
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-03 23:12:30 +0300

    Added Protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider, } from 'react-router-dom';
-import Layout from './routes/layout/layout';
+import { Layout, RequireAuth } from './routes/layout/layout';
 import HomePage from './routes/homePage/homePage';
 import ListPage from './routes/listPage/listPage';
 import SinglePage from './routes/singlePage/singlePage';
@@ -17,7 +17,7 @@ function App() {
             children: [
                 {
                     path: '/',
-                    element: <HomePage />
+                    element: <HomePage />,
                 },
                 {
                     path: '/list',
@@ -27,28 +27,35 @@ function App() {
                     path: '/:id',
                     element: <SinglePage />
                 },
+
+                {
+                    path: '/login',
+                    element: <Login />
+                },
+                {
+                    path: '/register',
+                    element: <Register />
+                },
+            ],
+        },
+        {
+            path: '/',
+            element: <RequireAuth />,
+            children: [
                 {
                     path: '/profile',
                     element: <ProfilePage />
                 },
                 {
                     path: '/profile/update',
-                    element: <ProfileUpdatePage />,
-                },
-                {
-                    path: '/register',
-                    element: <Register />
-                },
-                {
-                    path: '/login',
-                    element: <Login />
+                    element: <ProfileUpdatePage />
                 },
                 {
                     path: '/add',
-                    element: <NewPostPage />,
+                    element: <NewPostPage />
                 },
-            ]
-        }
+            ],
+        },
     ]);
 
     return (<RouterProvider router={router} />);
--- frontend/src/routes/layout/layout.jsx
+++ frontend/src/routes/layout/layout.jsx
@@ -1,5 +1,7 @@
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 import Navbar from '../../component/navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
 
 import './layout.scss';
 
@@ -16,4 +18,23 @@ function Layout() {
     );
 }
 
-export default Layout;
+function RequireAuth() {
+    const { currentUser } = useContext(AuthContext);
+
+    if (!currentUser) {
+        return <Navigate to="/login" />;
+    } else {
+        return (
+            <div className="layout">
+                <div className="navbar">
+                    <Navbar />
+                </div>
+                <div className="content">
+                    <Outlet />
+                </div>
+            </div>
+        );
+    }
+}
+
+export { Layout, RequireAuth };
