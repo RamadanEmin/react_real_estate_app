6e25d75d174e8bfed3fe010bc8fb43f4e7953e7f
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-04 22:37:02 +0300

    Added Delete user controller

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUsers, getUser, updateUser } from '../controllers/user.controller.js';
+import { getUsers, getUser, updateUser, deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', getUsers);
 router.get('/:id', verifyToken, getUser);
 router.put('/:id', verifyToken, updateUser);
+router.delete('/:id', verifyToken, deleteUser);
 
 export default router;
