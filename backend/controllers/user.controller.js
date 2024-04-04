5864b1ba92a62fbd99abdb6779b5cc3d2f863994
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-04 15:37:07 +0300

    Added Get user controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -9,3 +9,16 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ message: 'Failed to get users!' });
     }
 };
+
+export const getUser = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await prisma.user.findUnique({
+            where: { id }
+        });
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Failed to get user!' });
+    }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getUsers } from '../controllers/user.controller.js';
+import { getUsers, getUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+router.get('/:id', verifyToken, getUser);
 
 export default router;
