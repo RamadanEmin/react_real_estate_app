b610ee803abcade982f44a56a53a8d0cb8a60567
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-06 08:53:56 +0300

    Added Delete post controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -73,3 +73,27 @@ export const updatePost = async (req, res) => {
         res.status(500).json({ message: 'Failed to update post' });
     }
 };
+
+export const deletePost = async (req, res) => {
+    const id = req.params.id;
+    const tokenUserId = req.userId;
+
+    try {
+        const post = await prisma.post.findUnique({
+            where: { id }
+        });
+
+        if (post.userId !== tokenUserId) {
+            return res.status(403).json({ message: 'Not Authorized!' });
+        }
+
+        await prisma.post.delete({
+            where: { id }
+        });
+
+        res.status(200).json({ message: 'Post deleted' });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Failed to delete post' });
+    }
+};
