03020e3a4f9e0565e6c176f11e5f465e14e0b303
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-06 22:37:13 +0300

    Added List page loader

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import Register from './routes/register/register';
 import Login from './routes/login/login';
 import ProfileUpdatePage from './routes/profileUpdatePage/profileUpdatePage';
 import NewPostPage from './routes/newPostPage/newPostPage';
-import { singlePageLoader } from './lib/loaders';
+import { listPageLoader, singlePageLoader } from './lib/loaders';
 
 function App() {
     const router = createBrowserRouter([
@@ -22,7 +22,8 @@ function App() {
                 },
                 {
                     path: '/list',
-                    element: <ListPage />
+                    element: <ListPage />,
+                    loader: listPageLoader
                 },
                 {
                     path: '/:id',
