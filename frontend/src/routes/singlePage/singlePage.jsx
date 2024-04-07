96842808c4e3d5d97654fe689e7f572758ae8b30
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-07 16:07:25 +0300

    Added Save post functionality

--- frontend/src/routes/singlePage/singlePage.jsx
+++ frontend/src/routes/singlePage/singlePage.jsx
@@ -1,13 +1,34 @@
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import DOMPurify from 'dompurify';
 import Slider from '../../component/slider/Slider';
 import Map from '../../component/map/Map';
 
 import './singlePage.scss';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import apiRequest from '../../lib/apiRequest';
 
 function SinglePage() {
     const post = useLoaderData();
-    console.log(post);
+    const [saved, setSaved] = useState(post.isSaved);
+    const { currentUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+
+    const handleSave = async () => {
+        if (!currentUser) {
+            navigate('/login');
+        }
+
+        setSaved((prev) => !prev);
+        try {
+            await apiRequest.post('/users/save', { postId: post.id });
+        } catch (err) {
+            console.log(err);
+            setSaved((prev) => !prev);
+        }
+    };
+
     return (
         <div className="singlePage">
             <div className="details">
@@ -125,9 +146,14 @@ function SinglePage() {
                             <img src="/chat.png" alt="" />
                             Send a Message
                         </button>
-                        <button>
+                        <button
+                            onClick={handleSave}
+                            style={{
+                                backgroundColor: saved ? '#fece51' : 'white',
+                            }}
+                        >
                             <img src="/save.png" alt="" />
-                            Save the Place
+                            {saved ? 'Place Saved' : 'Save the Place'}
                         </button>
                     </div>
                 </div>
