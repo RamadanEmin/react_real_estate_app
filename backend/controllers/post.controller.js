34fa7188f38a7d6aca5e4058675262416f0d55b9
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-08 17:57:54 +0300

    Added Get profile posts controller

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -27,7 +27,6 @@ export const getPosts = async (req, res) => {
 
 export const getPost = async (req, res) => {
     const id = req.params.id;
-
     try {
         const post = await prisma.post.findUnique({
             where: { id },
@@ -56,15 +55,17 @@ export const getPost = async (req, res) => {
                         }
                     });
 
-                    res.status(200).json({ ...post, isSaved: saved ? true : false });
+                    return res.status(200).json({ ...post, isSaved: saved ? true : false });
                 }
+                
+                res.status(200).json({ ...post, isSaved: false });
             });
+        } else {
+            res.status(200).json({ ...post, isSaved: false });
         }
-
-        res.status(200).json({ ...post, isSaved: false });
     } catch (err) {
         console.log(err);
-        res.status(500).json({ message: 'Failed to get post' });
+        res.status(500).json({ message: "Failed to get post" });
     }
 };
 
