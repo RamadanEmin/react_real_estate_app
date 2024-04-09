91795999a7ce6b171f645db86ecdec4951ab2bcd
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-09 13:20:47 +0300

    Added Read chat controller

--- backend/routes/chat.route.js
+++ backend/routes/chat.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getChats, getChat, addChat } from '../controllers/chat.controller.js';
+import { getChats, getChat, addChat, readChat } from '../controllers/chat.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', verifyToken, getChats);
 router.get('/:id', verifyToken, getChat);
 router.post('/', verifyToken, addChat);
+router.put('/read/:id', verifyToken, readChat);
 
 export default router;
