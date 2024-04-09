bdad960289d59bab13273dbb888da2a8bb7a05ae
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-09 12:36:29 +0300

    Added Get chat controller

--- backend/controllers/chat.controller.js
+++ backend/controllers/chat.controller.js
@@ -36,6 +36,44 @@ export const getChats = async (req, res) => {
     }
 };
 
+export const getChat = async (req, res) => {
+    const tokenUserId = req.userId;
+
+    try {
+        const chat = await prisma.chat.findUnique({
+            where: {
+                id: req.params.id,
+                userIDs: {
+                    hasSome: [tokenUserId]
+                }
+            },
+            include: {
+                messages: {
+                    orderBy: {
+                        createdAt: 'asc'
+                    }
+                }
+            }
+        });
+
+        await prisma.chat.update({
+            where: {
+                id: req.params.id
+            },
+            data: {
+                seenBy: {
+                    push: [tokenUserId]
+                }
+            }
+        });
+
+        res.status(200).json(chat);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: 'Failed to get chat!' });
+    }
+};
+
 export const addChat = async (req, res) => {
     const tokenUserId = req.userId;
     
--- backend/routes/chat.route.js
+++ backend/routes/chat.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getChats, addChat } from '../controllers/chat.controller.js';
+import { getChats, getChat, addChat } from '../controllers/chat.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getChats);
+router.get('/:id', verifyToken, getChat);
 router.post('/', verifyToken, addChat);
 
 export default router;
