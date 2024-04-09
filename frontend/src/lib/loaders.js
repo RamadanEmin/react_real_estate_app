376cbd3dd2f2c930ba2077858998d14714ca7f53
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-09 15:14:17 +0300

    Update profile page with chat loader

--- frontend/src/lib/loaders.js
+++ frontend/src/lib/loaders.js
@@ -3,12 +3,14 @@ import apiRequest from './apiRequest';
 
 export const singlePageLoader = async ({ request, params }) => {
     const res = await apiRequest('/posts/' + params.id);
+
     return res.data;
 };
 
 export const listPageLoader = async ({ request, params }) => {
     const query = request.url.split('?')[1];
     const postPromise = apiRequest('/posts?' + query);
+    
     return defer({
         postResponse: postPromise
     });
@@ -16,7 +18,10 @@ export const listPageLoader = async ({ request, params }) => {
 
 export const profilePageLoader = async () => {
     const postPromise = apiRequest('/users/profilePosts');
+    const chatPromise = apiRequest('/chats');
+
     return defer({
-        postResponse: postPromise
+        postResponse: postPromise,
+        chatResponse: chatPromise
     });
 };
