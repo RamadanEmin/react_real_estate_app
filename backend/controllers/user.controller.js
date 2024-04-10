82d009a76094bf00b7c50fc984db4b9796e45d37
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-10 14:16:25 +0300

    Added Get notification number controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -123,7 +123,7 @@ export const profilePosts = async (req, res) => {
         const userPosts = await prisma.post.findMany({
             where: { userId: tokenUserId }
         });
-        
+
         const saved = await prisma.savedPost.findMany({
             where: { userId: tokenUserId },
             include: {
@@ -133,6 +133,30 @@ export const profilePosts = async (req, res) => {
 
         const savedPosts = saved.map((item) => item.post);
         res.status(200).json({ userPosts, savedPosts });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Failed to get profile posts!" });
+    }
+};
+
+export const getNotificationNumber = async (req, res) => {
+    const tokenUserId = req.userId;
+    
+    try {
+        const number = await prisma.chat.count({
+            where: {
+                userIDs: {
+                    hasSome: [tokenUserId]
+                },
+                NOT: {
+                    seenBy: {
+                        hasSome: [tokenUserId]
+                    }
+                }
+            }
+        });
+
+        res.status(200).json(number);
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: "Failed to get profile posts!" });
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,13 @@
 import express from 'express';
-import { getUsers, getUser, updateUser, deleteUser, savePost, profilePosts } from '../controllers/user.controller.js';
+import {
+    getUsers,
+    getUser,
+    updateUser,
+    deleteUser,
+    savePost,
+    profilePosts,
+    getNotificationNumber
+} from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
@@ -9,6 +17,7 @@ router.get('/', getUsers);
 router.put('/:id', verifyToken, updateUser);
 router.delete('/:id', verifyToken, deleteUser);
 router.post('/save', verifyToken, savePost);
-router.get("/profilePosts", verifyToken, profilePosts);
+router.get('/profilePosts', verifyToken, profilePosts);
+router.get('/notification', verifyToken, getNotificationNumber);
 
 export default router;
